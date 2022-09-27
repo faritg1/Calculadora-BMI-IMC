b3271bf7dca25d326138563d97d6d6c0c4e1f559
carlos torres <juanagelves@gmail.com>
2022-09-27 17:29:57 -0500

    terminado

--- main.js
+++ main.js
@@ -11,11 +11,57 @@ function conversion(){
         valor2 = valor2/100
         resultado = valor1/(valor2*valor2)
         document.getElementById("relt").innerHTML = resultado.toFixed(1)
+
+        if(resultado<=18.4)
+        {
+            document.getElementById("relt").style.backgroundColor="blue"
+        }
+        else if(resultado>=18.5 && resultado<=24.9)
+        {
+            document.getElementById("relt").style.backgroundColor="green"
+        }
+        else if(resultado>=25 && resultado<30)
+        {
+            document.getElementById("relt").style.backgroundColor="yellow"
+        }
+        else if(resultado>=30)
+        {
+            document.getElementById("relt").style.backgroundColor="red"
+        }
+
+        else if("relt" !== NaN){
+            document.getElementById("relt").innerHTML = ""
+            alert("No hay ningun valor")
+        }
+
+   
     }
     else{
 
         resultado = (valor1/(valor2*valor2))
         document.getElementById("relt").innerHTML = resultado.toFixed(1)
+
+        if(resultado<=18.4)
+        {
+            document.getElementById("relt").style.backgroundColor="blue"
+        }
+        else if(resultado>=18.5 && resultado<=24.9)
+        {
+            document.getElementById("relt").style.backgroundColor="green"
+        }
+        else if(resultado>=25 && resultado<30)
+        {
+            document.getElementById("relt").style.backgroundColor="yellow"
+        }
+        else if(resultado>=30)
+        {
+            document.getElementById("relt").style.backgroundColor="red"
+        }
+
+        else if("relt" !== NaN){
+            document.getElementById("relt").innerHTML = ""
+            alert("No hay ningun valor")
+        }
     }
 
 }
@@ -30,6 +76,8 @@ function reiniciar(){
     document.getElementById("peso").value = ""
     document.getElementById("altura").value = ""
     document.getElementById("relt").innerHTML = ""
+    document.getElementById("relt").style.backgroundColor = "transparent"
+
 
 }
 boton2.addEventListener('click',reiniciar)
